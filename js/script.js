ea2126558cb45b01d635d85f9cb9b542a273ad99
Rovshan Azimov <azi.rovshan>
2021-11-18 04:13:49 +0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    // dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  slidesPerView: 1.3,
+  spaceBetween: 16,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
